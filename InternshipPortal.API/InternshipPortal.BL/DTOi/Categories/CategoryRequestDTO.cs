07dd71200672f088d9849076af7c81d88679457a
JanJedvaj <jan.jedvaj2@gmail.com>
2026-01-01 16:33:10 +0100

    Add DTOs (Updated)

---
+++ InternshipPortal.API/InternshipPortal.BL/DTOi/Categories/CategoryRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternshipPortal.BL.DTOi.Categories
+{
+    public class CategoryRequestDTO
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ InternshipPortal.API/InternshipPortal.BL/DTOi/Internships/InternshipRequestDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InternshipPortal.BL.DTOi.Internships
+{
+    public class InternshipRequestDTO
+    {
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(500)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string FullDescription { get; set; } = string.Empty;
+
+        public bool IsFeatured { get; set; }
+        public bool Remote { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Location { get; set; } = string.Empty;
+
+        public DateTime? Deadline { get; set; }
+
+        [Required]
+        public int CompanyId { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
